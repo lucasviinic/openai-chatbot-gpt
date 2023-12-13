151aa57239ebee882cf1378777c7575c596f5240
Lucas Vinícius <62507586+lucasviinic@users.noreply.github.com>
2023-12-13 19:16:36 -0300

    feat: add history summarizer

--- views.py
+++ views.py
@@ -2,32 +2,21 @@ from app import app, bot
 from helpers import *
 from flask import render_template, request, Response
 from token_counter import *
+from summarizer import criando_resumo
 import os
 
 
-def limits_history(history: str, max_token_limit: int) -> str:
-    total_tokens = 0
-    partial_history = ''
-    for line in reversed(history.split('\n')):
-        line_tokens = token_counter(line)
-        total_tokens += line_tokens
-        if total_tokens > max_token_limit:
-            break
-        partial_history = line + partial_history
-    return partial_history
-
 def handle_response(prompt: str, history: str, file_name: str):
     partial_response = ''
-    max_token_limit = 2000
-    partial_history = limits_history(history, max_token_limit)
-    for response in bot(prompt, partial_history):
+    summary_history = criando_resumo(history)
+    for response in bot(prompt, summary_history):
         response_piece = response.choices[0].delta.get('content', '')
         if len(response_piece):
             partial_response += response_piece
             yield response_piece
 
     content = f"""
-    Histórico: {partial_history}
+    Histórico: {summary_history}
     Usuário: {prompt}
     IA: {partial_response}
     """
